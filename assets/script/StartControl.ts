29c7e8914ad29edf2e7452d65468f72c0c7ee021
chuanzhou_glb <chuanzhou1012@163.com>
2020-04-06 16:44:04 +0800

    添加预加载main场景

--- assets/script/StartControl.ts
+++ assets/script/StartControl.ts
@@ -3,6 +3,10 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class StartControl extends cc.Component {
 
+    protected onLoad(): void {
+        // 预加载main场景
+        cc.director.preloadScene('main');
+    }
 
     public goGameMain = (): void => {
         this.getComponent(cc.AudioSource).play();
